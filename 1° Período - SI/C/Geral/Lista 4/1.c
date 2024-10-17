e501f3fdedc8b1fc0ba13b41d92e2ba179781730
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2024-10-17 14:38:18 -0300

    Create 1.c

---
+++ 1° Período - SI/C/Geral/Lista 4/1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+int num, dig;
+
+printf("Digite um número: ");
+scanf("%d", &num);
+
+if (num < 0) {
+
+num = -num;
+
+}
+
+if (num == 0) {
+
+dig = 1;
+
+} else {
+
+dig = (int)log10(num) + 1;
+}
+
+printf("O número %d possui %d dígito(s).\n", num, dig);
+
+return 0;
+}
