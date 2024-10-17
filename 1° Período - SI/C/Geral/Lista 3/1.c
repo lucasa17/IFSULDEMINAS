03e907e61425d9fb8d74fbe350ef324a1e3520a0
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2024-10-17 14:29:20 -0300

    Create 1.c

---
+++ 1° Período - SI/C/Geral/Lista 3/1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+
+int main() {
+ for (int num=0; num<=20; num+=2){
+    printf ("%d \t", num);
+ }
+
+    return 0;
+}
