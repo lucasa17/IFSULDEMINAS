ba1f4d981a08c4d41acb39504b49406f29e94294
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2024-10-17 14:29:48 -0300

    Create 2.c

---
+++ 1° Período - SI/C/Geral/Lista 3/2.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    
+    for(int x = 11; x<=101; x += 10){
+        
+        printf("%d \t",x);
+        
+       
+    }
+
+    return 0;
+}
