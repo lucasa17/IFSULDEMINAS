4c26a9904faba89ab0e33289d54cdc1686eaf895
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2024-10-17 15:13:53 -0300

    Create area.c

---
+++ 1° Período - SI/C/Geral/area.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+
+int
+main ()
+{
+
+  double a, b, c;
+
+    scanf ("%lf", &a);
+
+    scanf ("%lf", &b);
+
+    scanf ("%lf", &c);
+
+  printf ("TRIANGULO: %.3lf\n", (a * c) / 2);
+
+  printf ("CIRCULO: %.3lf\n", 3.14159 * (c * c));
+
+  printf ("TRAPEZIO: %.3lf\n", ((a + b) * c) / 2);
+
+  printf ("QUADRADO: %.3lf\n", b * b);
+
+  printf ("RETANGULO: %.3lf\n", a * b);
+
+
+
+  return 0;
+}
