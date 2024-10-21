b8709c613b3debbe80152a86071901cb8c0ef2fe
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2024-10-21 18:55:02 -0300

    Update Matriz.cs
    Mais apresentável

--- 2º Período - SI/C#/Geral/Matriz.cs
+++ 2º Período - SI/C#/Geral/Matriz.cs
@@ -12,18 +12,28 @@ class HelloWorld {
         
         for(int j = 0; j < 3; j++){
             
+            Console.Write("["+i+"]["+j+"] = ");
             matriz[i,j] = int.Parse(Console.ReadLine());
                     
         }
         
     }
     
+    int cont = 0;
+    
     for(int i = 0; i < 3; i++){
         
         for(int j = 0; j < 3; j++){
             
-            Console.Write("["+i+"]["+j+"] = "+matriz[i,j]+" |  ");
-
+            if(cont % 3 == 0){
+                
+                Console.Write("\n");
+                
+            }
+    
+            Console.Write("["+i+"]["+j+"] = "+matriz[i,j]+"  ");
+            
+            cont++;
         }
         
     }
