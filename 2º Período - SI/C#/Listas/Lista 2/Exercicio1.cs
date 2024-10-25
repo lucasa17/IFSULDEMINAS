3d21486ffb92e003c1ffda4fe4d672e9140d13e1
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2024-10-25 10:08:17 -0300

    Update Exercicio1.cs

--- 2º Período - SI/C#/Listas/Lista 2/Exercicio1.cs
+++ 2º Período - SI/C#/Listas/Lista 2/Exercicio1.cs
@@ -7,12 +7,13 @@ using Matrizes;
 class Exercicio1
 {
 
-    static void maiorMatriz(int[,] matriz){
+    static void maiorMatriz(int[,] matriz)
+    {
 
         int linha = matriz.GetLength(0);
         int coluna = matriz.GetLength(1);
 
-        int maior = matriz[0,0];
+        int maior = matriz[0, 0];
 
         for (int i = 0; i < linha; i++)
         {
@@ -29,16 +30,35 @@ class Exercicio1
 
             }
         }
-    Console.WriteLine("\n\nMaior valor: "+maior);
+        Console.WriteLine("\n\nMaior valor: " + maior);
     }
 
 
-        static void Main()
+    static void Main()
     {
 
         Console.WriteLine("Matriz");
 
-        int[,] matriz = bibliotecaMatriz.randomMatriz();
+
+        volta:
+
+            Console.WriteLine("Obs: Máximo 100");
+
+            Console.Write("Digite a Quantidade de linhas da matriz: ");
+            int linha = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a Quantidade de linhas da matriz: ");
+            int coluna = int.Parse(Console.ReadLine());
+
+            if (linha > 100 || coluna > 100)
+            {
+                goto volta;
+
+            }
+
+        int[,] matriz = new int[linha, coluna];
+
+        bibliotecaMatriz.randomMatriz(matriz);
 
         bibliotecaMatriz.showMatriz(matriz);
 
@@ -48,4 +68,3 @@ class Exercicio1
     }
 
 }
-
