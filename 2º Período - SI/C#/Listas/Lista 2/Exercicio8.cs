efa8538af4cfd2229982cce1f70eacef78f68f39
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2024-11-01 09:19:29 -0300

    Update Exercicio8.cs

--- 2º Período - SI/C#/Listas/Lista 2/Exercicio8.cs
+++ 2º Período - SI/C#/Listas/Lista 2/Exercicio8.cs
@@ -5,56 +5,66 @@ using Matrizes;
 class Exercicio8
 {
 
-	
-	static void Main()
-	{
 
-		Console.WriteLine("Matriz");
+    static void Main()
+    {
 
-        volta:
+        Console.WriteLine("Matriz");
 
-		Console.WriteLine("\nDigite a quantidade de raios:");
-		Console.Write("Obs: Máximo 500000 e Minimo 2\n");
-		
+    volta:
+
+        Console.WriteLine("\nDigite a quantidade de raios:");
+        Console.Write("Obs: Máximo 500000 e Minimo 2\n");
+
         int raios = int.Parse(Console.ReadLine());
 
-    		if (raios < 2 || raios > 500000)
-    		{
-    			goto volta;
-    
-    		}
-    		
+        if (raios < 1 && raios > 500001)
+        {
+            goto volta;
 
-		int[,] matriz = new int[501, 501];
+        }
 
 
-        for(int i = 0; i < 501; i++){
-            
-            for(int j = 0; j < 501; j++){
-                
-                matriz[i,j] = 0;
+        int[,] matriz = new int[501, 501];
+
+
+        for (int i = 0; i < 501; i++)
+        {
+
+            for (int j = 0; j < 501; j++)
+            {
+
+                matriz[i, j] = 0;
             }
         }
-        
-        for(int k = 0; k < raios; k++){
-            
-            Console.WriteLine("\nDigite as coordenadas: ");
-            Console.Write("Linha:");
-            int linha = int.Parse(Console.ReadLine());
-            
-            Console.Write("Coluna:");
-            int coluna = int.Parse(Console.ReadLine());
-            
-                if(matriz[linha,coluna] == 0){
-                    
-                    matriz[linha,coluna] = 1;
-                    
-                }
-                else if(matriz[linha,coluna] == 1){
-                    
-                    Console.WriteLine("Um raio caiu no mesmo local duas vezes.");
-                    return;
-                }
-        }      
-	}
+
+        Console.WriteLine("\nDigite as coordenadas: ");
+
+        for (int k = 0; k < raios; k++)
+        {
+
+
+            var lincol = Console.ReadLine().Split();
+
+            int linha = int.Parse(lincol[0]);
+            int coluna = int.Parse(lincol[1]);
+
+            if (matriz[linha, coluna] == 0)
+            {
+
+                matriz[linha, coluna] = 1;
+
+            }
+            else if (matriz[linha, coluna] == 1)
+            {
+
+                Console.WriteLine("2 raios caíram no mesmo local");
+                return;
+            }
+
+        }
+
+        Console.Write("Nenhum raio caiu no mesmo lugar");
+
+    }
 }
