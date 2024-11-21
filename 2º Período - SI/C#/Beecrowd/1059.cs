8b2098ccfb410af5d95a1b8139107ac07752c2f8
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2024-11-21 16:17:53 -0300

    Create 1059.cs

---
+++ 2º Período - SI/C#/Beecrowd/1059.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+
+    static void Main()
+    {
+
+        for(int a = 1; a <= 100; a++){
+            
+            if(a %2 == 0){
+                
+                Console.WriteLine($"{a}");
+                
+            }
+        }
+
+    }
+}
