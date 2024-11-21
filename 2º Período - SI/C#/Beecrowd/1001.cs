dd1d9dd664e7fba256ff0679ea861d2adf78bab4
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2024-11-21 16:23:10 -0300

    Create 1001.cs

---
+++ 2º Período - SI/C#/Beecrowd/1001.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+
+    static void Main()
+    {
+
+        int A, B;
+        
+        A = int.Parse(Console.ReadLine());
+
+        B = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"X = {A + B}");
+
+    }
+}
