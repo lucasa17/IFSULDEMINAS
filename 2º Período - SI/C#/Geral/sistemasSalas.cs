f0935a74fb0bee594bb1f07354fdebc78379efa4
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2025-02-06 13:18:39 -0300

    Update sistemasSalas.cs

--- 2º Período - SI/C#/Geral/sistemasSalas.cs
+++ 2º Período - SI/C#/Geral/sistemasSalas.cs
@@ -42,9 +42,11 @@ class Program
 
             Console.WriteLine("\nSala " + (i + 1));
 
-            Console.WriteLine("Localização: " + listaSalas[i].numeroSala);
+            Console.WriteLine("Número da Sala: " + listaSalas[i].numeroSala);
 
-            Console.WriteLine("Tipo: " + listaSalas[i].localizacao);
+            Console.Write("Tipo da Sala: " + listaSalas[i].tipo);
+
+            Console.WriteLine("Localização: " + listaSalas[i].localizacao);
 
             Console.WriteLine("Tipo da Sala: " + listaSalas[i].tipo);
 
@@ -58,28 +60,142 @@ class Program
     static void mostraSalaNumero(List<TipoSalaAula> listaSalas, int numero)
     {
 
-        Console.WriteLine("Salas:\n");
-
         for (int i = 0; i < listaSalas.Count; i++)
         {
             if (listaSalas[i].numeroSala == numero)
             {
-
+                
                 Console.WriteLine("\nSala " + (i + 1));
-
-                Console.WriteLine("Localização: " + listaSalas[i].numeroSala);
-
-                Console.WriteLine("Tipo: " + listaSalas[i].localizacao);
-
+    
+                Console.WriteLine("Número da Sala: " + listaSalas[i].numeroSala);
+    
+                Console.Write("Tipo da Sala: " + listaSalas[i].tipo);
+    
+                Console.WriteLine("Localização: " + listaSalas[i].localizacao);
+    
                 Console.WriteLine("Tipo da Sala: " + listaSalas[i].tipo);
-
+    
                 Console.WriteLine("Tamanho (m²): " + listaSalas[i].tamanho);
-
+    
                 Console.WriteLine("Quantidade de Cadeiras: " + listaSalas[i].quantidadeCadeiras);
 
             }
         }
     }
+    
+    static void mostraSalaTamanho(List<TipoSalaAula> listaSalas, double tamanho)
+    {
+
+        Console.WriteLine("Salas:\n");
+
+        for (int i = 0; i < listaSalas.Count; i++)
+        {
+            if (listaSalas[i].tamanho >= tamanho)
+            {
+                
+                Console.WriteLine("\nSala " + (i + 1));
+    
+                Console.WriteLine("Número da Sala: " + listaSalas[i].numeroSala);
+    
+                Console.Write("Tipo da Sala: " + listaSalas[i].tipo);
+    
+                Console.WriteLine("Localização: " + listaSalas[i].localizacao);
+    
+                Console.WriteLine("Tipo da Sala: " + listaSalas[i].tipo);
+    
+                Console.WriteLine("Tamanho (m²): " + listaSalas[i].tamanho);
+    
+                Console.WriteLine("Quantidade de Cadeiras: " + listaSalas[i].quantidadeCadeiras);
+
+            }
+        }
+    }
+    
+    static void maiorSala(List<TipoSalaAula> listaSalas){
+        
+        double tamanho = listaSalas[0].tamanho;
+        
+        int cont = 0;
+        
+            for (int i = 0; i < listaSalas.Count; i++)
+            {
+            
+                if(listaSalas[i].tamanho > tamanho){
+                    
+                    cont = i;
+                    
+                }
+                
+            }
+        
+                Console.WriteLine("\nMaior Sala em metragem é a " + cont);
+    
+                Console.WriteLine("Número da Sala: " + listaSalas[cont].numeroSala);
+    
+                Console.Write("Tipo da Sala: " + listaSalas[cont].tipo);
+    
+                Console.WriteLine("Localização: " + listaSalas[cont].localizacao);
+    
+                Console.WriteLine("Tipo da Sala: " + listaSalas[cont].tipo);
+    
+                Console.WriteLine("Tamanho (m²): " + listaSalas[cont].tamanho);
+    
+                Console.WriteLine("Quantidade de Cadeiras: " + listaSalas[cont].quantidadeCadeiras);
+
+        
+    }
+    
+    static void mostraSalaCadeiras(List<TipoSalaAula> listaSalas, int cadeiras)
+    {
+
+        Console.WriteLine("Salas:\n");
+
+        for (int i = 0; i < listaSalas.Count; i++)
+        {
+            if (listaSalas[i].quantidadeCadeiras >= cadeiras)
+            {
+                
+                Console.WriteLine("\nSala " + (i + 1));
+    
+                Console.WriteLine("Número da Sala: " + listaSalas[i].numeroSala);
+    
+                Console.Write("Tipo da Sala: " + listaSalas[i].tipo);
+    
+                Console.WriteLine("Localização: " + listaSalas[i].localizacao);
+    
+                Console.WriteLine("Tipo da Sala: " + listaSalas[i].tipo);
+    
+                Console.WriteLine("Tamanho (m²): " + listaSalas[i].tamanho);
+    
+                Console.WriteLine("Quantidade de Cadeiras: " + listaSalas[i].quantidadeCadeiras);
+
+            }
+        }
+    }
+    
+    
+    static void geraRelatorio(List<TipoSalaAula> listaSalas)
+    {
+
+        double tamanho = 0;
+
+        Console.WriteLine("Relatório:\n");
+
+        for (int i = 0; i < listaSalas.Count; i++)
+        {
+            
+            tamanho += listaSalas[i].tamanho;
+
+
+        }
+        
+            Console.WriteLine("Número de salas: " + (listaSalas.Count));
+
+            Console.WriteLine("Tamanho total das salas(m²): " + tamanho);
+
+    }
+    
+    
     static void salvarDados(List<TipoSalaAula> listaSalas, string nomeArquivo)
     {
         StreamWriter writer = new StreamWriter(nomeArquivo);
@@ -121,6 +237,10 @@ class Program
         Console.WriteLine("1 - Adicionar Sala");
         Console.WriteLine("2 - Mostrar Salas");
         Console.WriteLine("3 - Buscar sala por número");
+        Console.WriteLine("4 - Buscar sala por tamanho");
+        Console.WriteLine("5 - Buscar maior sala");
+        Console.WriteLine("6 - Buscar sala por quantidades de cadeiras");
+        Console.WriteLine("7 - Gerar relatório");
         Console.WriteLine("0 - Sair");
         Console.Write("Opção: ");
         return int.Parse(Console.ReadLine());
@@ -158,6 +278,39 @@ class Program
 
                     break;
 
+                case 4:
+
+                    Console.WriteLine("Digite o tamanho: ");
+
+                    double tamanho = double.Parse(Console.ReadLine());
+
+                    mostraSalaTamanho(listaSalas, tamanho);
+
+                    break;
+
+                case 5:
+
+                    maiorSala(listaSalas);
+
+                    break;
+                
+                case 6:
+
+                    Console.WriteLine("Digite a quantidade minima de cadeiras: ");
+
+                    int cadeiras = int.Parse(Console.ReadLine());
+
+                    mostraSalaCadeiras(listaSalas, cadeiras);
+
+                    break;
+                    
+                    
+                case 7:
+
+                    geraRelatorio(listaSalas);
+
+                    break;
+                    
                 case 0:
 
                     Console.WriteLine("Saindo...");
