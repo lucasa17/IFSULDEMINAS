0f73b834d9a66e656adec139f33efcad1bd9d3c9
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2025-03-13 10:54:33 -0300

    Create Car.java

---
+++ 3º Período - SI/Java/Geral/teste/Car.java
@@ -0,0 +1,14 @@
+package teste;
+
+public class Car{
+
+private int velocidade;
+public int velocidadeAtual() {
+return this.velocidade;
+}
+
+public void alteraVelocidade(int novaVelocidade) {
+this.velocidade = novaVelocidade;
+}
+
+}
