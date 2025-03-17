2831bafabb228c4cfdd5bcab1d158010b4b71772
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2025-03-17 08:27:50 -0300

    Create somaMatrizes.java

---
+++ 3º Período - SI/Java/Geral/somaMatrizes.java
@@ -0,0 +1,79 @@
+package aula;
+
+import java.util.Scanner;
+
+public class somaMatrizes {
+  
+    public static void preencheMatriz(int[][] matriz){
+        
+        Scanner scanner = new Scanner(System.in);
+
+        for(int i = 0; i < 3; i++){
+
+           for(int j = 0; j < 3; j++){
+               
+                System.out.print("Elemento [" + i + "][" + j + "]: ");
+                matriz[i][j] = scanner.nextInt();               
+           }
+
+        }
+
+    }
+    
+    
+    public static int[][] somaMatriz(int[][] matriz1, int[][] matriz2){
+        
+        int[][] matriz3 = new int[3][3];
+        
+         for(int i = 0; i < 3; i++){
+
+           for(int j = 0; j < 3; j++){
+               
+                System.out.print("Elemento [" + i + "][" + j + "]: ");
+                
+                matriz3[i][j] = matriz1[i][j] + matriz2[i][j];
+                
+           }
+
+        }
+         
+    return matriz3;  
+    }
+    
+    
+     public static void imprimeMatriz(int[][] matriz){
+        
+        for(int i = 0; i < 3; i++){
+
+           for(int j = 0; j < 3; j++){
+               
+                System.out.print("Elemento [" + i + "][" + j + "]: ");
+                System.out.println(matriz[i][j]);
+           }
+
+        }
+
+    }
+    
+    
+    
+    public static void main(String[] args){
+                
+        int[][] matriz1 = new int[3][3];
+        int[][] matriz2 = new int[3][3];
+        int[][] matriz3 = new int[3][3];
+        
+        System.out.println("Digite a matriz 1:");
+        preencheMatriz(matriz1);
+        
+        System.out.println("\nDigite a matriz 2:");
+        preencheMatriz(matriz2);
+                
+        matriz3 = somaMatriz(matriz1, matriz2);
+        
+        System.out.println("\nSoma das matrizes:");
+        imprimeMatriz(matriz3);
+
+    }
+    
+}
