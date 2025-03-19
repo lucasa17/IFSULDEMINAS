b6d5d72a19cf1ef3a4f42451f00b9170fb468574
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2025-03-19 09:45:55 -0300

    Update teste.java

--- 3º Período - SI/Java/Geral/teste/teste.java
+++ 3º Período - SI/Java/Geral/teste/teste.java
@@ -1,32 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package teste;
 
-/**
- *
- * @author 41279517883
- */
+
 public class Teste {
 
+    
+    static int addVelocidade(int velocidade){
+        
+        velocidade++;
+        
+        System.out.println("Velocidade na função: "+ velocidade);
+       
+    return velocidade;
+    }
+    
+    
+    static void addVelocidadeCar(Car carro){
+        
+        int novaVelocidade = carro.velocidadeAtual();
+        
+        novaVelocidade++;
+        
+        carro.alteraVelocidade(novaVelocidade);
+        
+        System.out.println("\nVelocidade atual alterada na função: "+novaVelocidade);
+    }
+    
     
     public static void main(String[] args) {
 
         Car novoCarro = new Car();//novoCarro se torna objeto pois recebe referencia de Car
-        
-        Car novoCarro2 = novoCarro;//novoCarro2 é um ponteiro, apenas copia novoCarro
 
             novoCarro.alteraVelocidade(10);
-                
-        System.out.println("Velocidade do novoCarro: "+ novoCarro.velocidadeAtual());
-        
-            novoCarro2.alteraVelocidade(20);
-        
-        System.out.println("Velocidade do novoCarro2: "+ novoCarro2.velocidadeAtual());
 
+        Car novaCarroca = new Car();//novoCarro se torna objeto pois recebe referencia de Car
+            
+            novaCarroca.alteraVelocidade(15);
+        
+
+        
         System.out.println("Velocidade do novoCarro: "+ novoCarro.velocidadeAtual());
+
+            int velocidade = addVelocidade(novoCarro.velocidadeAtual());
+        
+        System.out.println("Velocidade retornada pela função: "+velocidade);
+
+        System.out.println("Velocidade fora da função: "+novoCarro.velocidadeAtual());
+
+
+        
+            addVelocidadeCar(novoCarro);
+            
+        System.out.println("Velocidade fora da nova função: "+novoCarro.velocidadeAtual());
+
+            addVelocidadeCar(novaCarroca);
+
+        System.out.println("Velocidade fora da nova função: "+novaCarroca.velocidadeAtual());
 
     }
     
