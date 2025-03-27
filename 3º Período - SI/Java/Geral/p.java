16bd48a07c9dfd126884320bc70d691cc1c83b77
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2025-03-27 09:44:12 -0300

    Create p.java

---
+++ 3º Período - SI/Java/Geral/p.java
@@ -0,0 +1,22 @@
+
+package p;
+
+
+public class p {
+
+    public static void main(String[] args) {
+
+        Pessoa p1 = new Pessoa();
+        
+       
+        
+        p1.setCpf(23121);
+        
+        System.out.println(p1.getCpf());
+        
+        
+
+
+    }
+    
+}
