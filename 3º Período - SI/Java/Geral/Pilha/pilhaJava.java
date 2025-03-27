3193cb50509f2f0490dcd619db18989b65687006
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2025-03-27 10:42:58 -0300

    Update pilhaJava.java

--- 3º Período - SI/Java/Geral/Pilha/pilhaJava.java
+++ 3º Período - SI/Java/Geral/Pilha/pilhaJava.java
@@ -7,7 +7,6 @@ public class PilhaJava {
 static Scanner leia = new Scanner(System.in);
 
     private static int menu() {
-            
         
             System.out.println("\n--- Menu Pilha ---");
             System.out.println("1. Inserir elemento");
@@ -24,35 +23,42 @@ static Scanner leia = new Scanner(System.in);
         
         System.out.print("Tamanho da pilha: ");
         int tamanho = leia.nextInt();
-        
         Pilha<Integer> minhaPilha = new Pilha(tamanho);
 
         int op;
-        
         do{
-            
             op = menu();
-            
             switch(op){
                 
-                case 1:
-                    
+                case 1: 
                     System.out.print("Dado: ");
                     int dado = leia.nextInt();
                     
-                    minhaPilha.push(dado);
-                
+                    if(minhaPilha.push(dado) == true){
+                        System.out.println("Item inserido com sucesso");
+                        break;
+                    }
+                    System.out.println("Pilha cheia, Item não inserido");
                 break;
                 
-                case 4:
-                    
-                    System.out.println(minhaPilha);
-                
+                case 2: 
+                    if(minhaPilha.isEmpty() == true){
+                    System.out.println("\nNão há itens para remover");        
+                    break;
+                    }
+                    System.out.println("\nO item " + minhaPilha.pop() + " foi removido do topo");
                 break;
+                
+                case 3: 
+                    System.out.println("\nTopo = " + minhaPilha.peek());  
+                break;  
+                
+                case 4: 
+                    System.out.println("\n" + minhaPilha);
+                break; 
                 
             }
-            
+            leia.nextLine();//Pausa
         }while(op != 0);
-    }
-    
+    } 
 }
