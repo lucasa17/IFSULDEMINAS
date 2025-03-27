488caf3b047c9c350841264c2da2b07ac3fa2550
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2025-03-27 09:43:44 -0300

    Create Pessoa.java

---
+++ 3º Período - SI/Java/Geral/Pessoa.java
@@ -0,0 +1,31 @@
+package baguio.ai;
+
+public class Pessoa {
+    
+    
+    private Integer cpf;
+    private String name;
+
+
+    public int getCpf(){
+        
+        
+    return this.cpf; 
+    }
+    
+    public void setCpf(int cpf){
+        
+        this.cpf = cpf;
+        
+    }
+    
+    
+    public Pessoa(){
+        
+        
+        
+    }
+    
+    
+
+}
