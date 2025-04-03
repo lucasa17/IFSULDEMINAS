9e733c8a105f970f9d78b505d545566742898dca
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2025-04-03 08:00:47 -0300

    Update Pilha.java

--- 3º Período - SI/Java/Geral/Atividades /Recordes/Pilha.java
+++ 3º Período - SI/Java/Geral/Atividades /Recordes/Pilha.java
@@ -23,11 +23,11 @@ public class Pilha<T> {
     
     
     public boolean push(T dado){
-        if(!this.isFull()){
+        if(this.isFull()){
+            aumentaCapacidade();
+        }
             this.elementos[++this.topo] = dado;
-            return true;
-        }// fim if
-        return false;
+    return true;
     }
     
     public T pop(){
@@ -42,6 +42,16 @@ public class Pilha<T> {
         return this.elementos[this.topo];
     }
     
+    private void aumentaCapacidade() {
+        if (this.isFull()) { 
+            T[] elementosNovos = (T[]) new Object[this.elementos.length * 2]; 
+            for(int i = 0; i < this.elementos.length; i++) {
+                elementosNovos[i] = this.elementos[i]; 
+            
+        }
+        this.elementos = elementosNovos;
+    }
+}
     @Override
     public String toString(){
         
