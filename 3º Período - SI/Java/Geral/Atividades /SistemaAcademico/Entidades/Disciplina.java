9973a80025a0a03de015857c4660b6de3df1dc3b
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2025-04-07 07:14:48 -0300

    Create Disciplina.java

---
+++ 3º Período - SI/Java/Geral/Atividades /SistemaAcademico/Entidades/Disciplina.java
@@ -0,0 +1,21 @@
+
+package Entidades;
+
+public class Disciplina {
+    
+    public String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Disciplina(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
