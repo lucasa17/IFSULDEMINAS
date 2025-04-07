c658ba9ced4f1fddbbfc3567653e2afff978c892
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2025-04-07 08:28:15 -0300

    Create Program.java

---
+++ 3º Período - SI/Java/ListaAlvaro/Program.java
@@ -0,0 +1,22 @@
+package alvaro;
+
+public class Program {
+
+    public static void main(String[] args) {
+        System.out.println("\nAtt 1");  
+        //att1.helloWorld();
+        System.out.println("\nAtt 2");
+        //att2.soma();
+        System.out.println("\nAtt 3");
+        //att3.par();
+        System.out.println("\nAtt 4");
+        //att4.fatorial();
+        System.out.println("\nAtt 5");
+        //att5.maior();
+        System.out.println("\nAtt 6");
+        //att6.tabuada();
+        System.out.println("\nAtt 6");
+        att7.fibonacci();
+    }
+    
+}
