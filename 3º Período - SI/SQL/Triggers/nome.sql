b9e660322d4223f93c4396313ac7ca2c00f42c45
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2025-05-27 10:50:44 -0300

    Update nome.sql

--- 3º Período - SI/SQL/Triggers/nome.sql
+++ 3º Período - SI/SQL/Triggers/nome.sql
@@ -14,3 +14,25 @@ insert into cliente (nome, cpf, rg, dataNascimento, telefone)
 	values ('Joana', 6464, 7327837, '2019-02-02', 26725);
 
 select * from cliente order by idCLIENTE desc;
+
+delimiter //
+create trigger tr_atualiza_data_alteracao_cpf 
+	before update on cliente
+    for each row
+    begin
+		if (new.cpf != old.cpf) then
+			update contavinculada 
+            set dataAbertura = now()
+            where old.idCLIENTE = CLIENTE_idCLIENTE;
+		end if;
+	end//
+delimiter ;
+
+select * from contavinculada;
+select * from cliente;
+
+update cliente 
+set cpf = '123.123.123-12'
+where idCLIENTE = 1;
+
+
