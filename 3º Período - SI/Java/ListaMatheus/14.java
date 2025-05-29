b9893dcf67e3efdc574abfc0d57cb2e320e4d135
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2025-05-28 21:02:57 -0300

    Update 14.java

--- 3º Período - SI/Java/ListaMatheus/14.java
+++ 3º Período - SI/Java/ListaMatheus/14.java
@@ -1,1 +1,16 @@
-14
+public boolean contemElemento(int elemento) {
+    if (inicio == fim){
+      return false;
+    }  
+    int i = inicio;
+    while (i != fim) {
+        if (dados[i] == elemento) {
+            return true;
+        }
+        i++;
+        if (i == tamanho) {
+            i = 0;
+        }
+    }
+    return false;
+}
