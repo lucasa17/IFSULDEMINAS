d3490679900661c3c5ffecb2e1bedcd40ac99a5a
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2025-07-16 21:06:37 -0300

    Update 4.java

--- 3º Período - SI/Java/ListaMatheus17-07/4.java
+++ 3º Período - SI/Java/ListaMatheus17-07/4.java
@@ -1,11 +1,7 @@
     public static int somaVetorRecursiva(int[] vetor, int n) {
-        /**
-         * Retorna a soma dos elementos de um vetor de n elementos usando recursividade.
-         */
         if (n <= 0) {
-            return 0; // Caso base: vetor vazio ou tamanho inválido, a soma é 0
+            return 0; 
         } else {
-            // Soma o último elemento (índice n-1) com a soma recursiva do restante do vetor
             return vetor[n - 1] + somaVetorRecursiva(vetor, n - 1);
         }
     }
