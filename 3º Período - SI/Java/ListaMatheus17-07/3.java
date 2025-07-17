c3ad0b18cb85f48e11cd14e7adc986c725d32151
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2025-07-16 21:06:55 -0300

    Update 3.java

--- 3º Período - SI/Java/ListaMatheus17-07/3.java
+++ 3º Período - SI/Java/ListaMatheus17-07/3.java
@@ -1,5 +1,4 @@
 public void removerElementosPares() {
-        // Remover nós da cabeça (head) que são pares
         while (this.head != null && this.head.data % 2 == 0) {
             this.head = this.head.next;
         }
@@ -7,9 +6,9 @@ public void removerElementosPares() {
         Node current = this.head;
         while (current != null && current.next != null) {
             if (current.next.data % 2 == 0) {
-                current.next = current.next.next; // Remove o próximo nó
+                current.next = current.next.next; 
             } else {
-                current = current.next; // Avança para o próximo nó
+                current = current.next; 
             }
         }
     }
