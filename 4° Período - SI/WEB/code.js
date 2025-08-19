f66eb8b2fa06106959b838ecbf14c44cd252a0ad
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2025-08-19 10:46:06 -0300

    Update code.js

--- 4° Período - SI/WEB/code.js
+++ 4° Período - SI/WEB/code.js
@@ -20,8 +20,23 @@ function searchAPI(){
 
     // Requisição HTTP GET {request -> SERVER -> response}
     fetch(users_url)
-        .then(reponse => {
-            
-        }
-        )
+        .then(response => {
+            if(!response.ok){
+                throw new Error("Erro ao consultar API.");
+            }
+            return response.json();
+        }).then(data =>{
+            const p = document.createElement('p');
+            const index = Math.floor(Math.random() * 9);
+            const text = document.createTextNode("Nome: "+ data[index].name + "Email: "+ data[index].email);
+            p.appendChild(text);
+            document.body.appendChild(p);        
+        }).catch(error => {
+            const p = document.createElement('p');
+            const textNode = document.createTextNode(error.message);
+            p.appendChild(textNode);
+            document.body.appendChild(p);   
+        });
 }
+
+buttonRemoteQuery.addEventListener('click', searchAPI);
