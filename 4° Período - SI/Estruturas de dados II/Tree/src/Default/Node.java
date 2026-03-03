374dc0b06275ad7fdd08b753d14eb6c71140a645
Lucas Silva da Rosa <134516774+lucasa17@users.noreply.github.com>
2026-03-03 08:11:26 -0300

    Add files via upload

---
+++ 4° Período - SI/Estruturas de dados II/Tree/src/Default/Node.java
@@ -0,0 +1,14 @@
+package Default;
+
+public class Node<T extends Comparable<T>>{
+    T dado;
+    int fb;
+    Node<T> esquerda;
+    Node<T> direita;
+    public Node(T dado){
+        this.dado = dado;
+        this.esquerda = null;
+        this.direita = null;
+        this.fb = 0;
+    }
+}
